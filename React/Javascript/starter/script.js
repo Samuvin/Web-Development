9e5b02ed1d864c42f1a632f1928c5ca5da418716
admin <samuvin.j@gmail.com>
2024-06-07 12:27:17 +0530

    Update

---
+++ React/Javascript/starter/script.js
@@ -0,0 +1,201 @@
+const data = [
+	{
+		id: 1,
+		title: "The Lord of the Rings",
+		publicationDate: "1954-07-29",
+		author: "J. R. R. Tolkien",
+		genres: [
+			"fantasy",
+			"high-fantasy",
+			"adventure",
+			"fiction",
+			"novels",
+			"literature",
+		],
+		hasMovieAdaptation: true,
+		pages: 1216,
+		translations: {
+			spanish: "El señor de los anillos",
+			chinese: "魔戒",
+			french: "Le Seigneur des anneaux",
+		},
+		reviews: {
+			goodreads: {
+				rating: 4.52,
+				ratingsCount: 630994,
+				reviewsCount: 13417,
+			},
+			librarything: {
+				rating: 4.53,
+				ratingsCount: 47166,
+				reviewsCount: 452,
+			},
+		},
+	},
+	{
+		id: 2,
+		title: "The Cyberiad",
+		publicationDate: "1965-01-01",
+		author: "Stanislaw Lem",
+		genres: [
+			"science fiction",
+			"humor",
+			"speculative fiction",
+			"short stories",
+			"fantasy",
+		],
+		hasMovieAdaptation: false,
+		pages: 295,
+		translations: {},
+		reviews: {
+			goodreads: {
+				rating: 4.16,
+				ratingsCount: 11663,
+				reviewsCount: 812,
+			},
+			librarything: {
+				rating: 4.13,
+				ratingsCount: 2434,
+				reviewsCount: 0,
+			},
+		},
+	},
+	{
+		id: 3,
+		title: "Dune",
+		publicationDate: "1965-01-01",
+		author: "Frank Herbert",
+		genres: ["science fiction", "novel", "adventure"],
+		hasMovieAdaptation: true,
+		pages: 658,
+		translations: {
+			spanish: "",
+		},
+		reviews: {
+			goodreads: {
+				rating: 4.25,
+				ratingsCount: 1142893,
+				reviewsCount: 49701,
+			},
+		},
+	},
+	{
+		id: 4,
+		title: "Harry Potter and the Philosopher's Stone",
+		publicationDate: "1997-06-26",
+		author: "J. K. Rowling",
+		genres: ["fantasy", "adventure"],
+		hasMovieAdaptation: true,
+		pages: 223,
+		translations: {
+			spanish: "Harry Potter y la piedra filosofal",
+			korean: "해리 포터와 마법사의 돌",
+			bengali: "হ্যারি পটার এন্ড দ্য ফিলোসফার্স স্টোন",
+			portuguese: "Harry Potter e a Pedra Filosofal",
+		},
+		reviews: {
+			goodreads: {
+				rating: 4.47,
+				ratingsCount: 8910059,
+				reviewsCount: 140625,
+			},
+			librarything: {
+				rating: 4.29,
+				ratingsCount: 120941,
+				reviewsCount: 1960,
+			},
+		},
+	},
+	{
+		id: 5,
+		title: "A Game of Thrones",
+		publicationDate: "1996-08-01",
+		author: "George R. R. Martin",
+		genres: ["fantasy", "high-fantasy", "novel", "fantasy fiction"],
+		hasMovieAdaptation: true,
+		pages: 835,
+		translations: {
+			korean: "왕좌의 게임",
+			polish: "Gra o tron",
+			portuguese: "A Guerra dos Tronos",
+			spanish: "Juego de tronos",
+		},
+		reviews: {
+			goodreads: {
+				rating: 4.44,
+				ratingsCount: 2295233,
+				reviewsCount: 59058,
+			},
+			librarything: {
+				rating: 4.36,
+				ratingsCount: 38358,
+				reviewsCount: 1095,
+			},
+		},
+	},
+];
+
+function getBooks() {
+	return data;
+}
+
+function getBook(id) {
+	return data.find((d) => d.id === id);
+}
+
+//Destructuring
+const books = getBook(2);
+books;
+const { title, author, pages, publicationDate, genres, hasMovieAdaptation } =
+	books;
+
+const [primaryGenre, secondayGenre] = genres;
+primaryGenre;
+
+//Rest Operator
+
+const [a, b, ...otherGenres] = genres; // should be place at the end rest element
+otherGenres;
+
+//Spread Operator
+//const newGenres = [genres, "epic fantasy"]; // create a 2d array
+const newGenres = [...genres, "epic fantasy"];
+newGenres;
+
+//spread operator in objects
+const updateBook = {
+	...books,
+	moviepublicationDate: "2001-12-19",
+	pages: 1123,
+};
+updateBook;
+
+//template litreals
+const summary = `${title} , ${pages}-page long book ,was written by ${author} in ${
+	publicationDate.split("-")[0]
+}`;
+summary;
+
+//Ternary
+const pagesRange = pages > 1000 ? "over a thousand" : "less than 100";
+pagesRange;
+
+const summary2 = `${title} , ${pages}-page long book ,was written by ${author} in ${
+	publicationDate.split("-")[0]
+}.The book has ${hasMovieAdaptation ? "" : "not"} been adapted as a movie`;
+
+summary2;
+
+//promise
+// fetch("https://jsonplaceholder.typicode.com/todos")
+// 	.then((res) => res.json())
+// 	.then((data) => console.log(data));
+
+//async await
+
+async function getTodos() {
+	const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+	const data = await res.json();
+	console.log(data);
+}
+getTodos();
---
+++ React/pizza_menu/src/index.js
@@ -0,0 +1,144 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+const pizzaData = [
+	{
+		name: "Focaccia",
+		ingredients: "Bread with italian olive oil and rosemary",
+		price: 6,
+		photoName: "pizzas/focaccia.jpg",
+		soldOut: false,
+	},
+	{
+		name: "Pizza Margherita",
+		ingredients: "Tomato and mozarella",
+		price: 10,
+		photoName: "pizzas/margherita.jpg",
+		soldOut: false,
+	},
+	{
+		name: "Pizza Spinaci",
+		ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+		price: 12,
+		photoName: "pizzas/spinaci.jpg",
+		soldOut: false,
+	},
+	{
+		name: "Pizza Funghi",
+		ingredients: "Tomato, mozarella, mushrooms, and onion",
+		price: 12,
+		photoName: "pizzas/funghi.jpg",
+		soldOut: false,
+	},
+	{
+		name: "Pizza Salamino",
+		ingredients: "Tomato, mozarella, and pepperoni",
+		price: 15,
+		photoName: "pizzas/salamino.jpg",
+		soldOut: true,
+	},
+	{
+		name: "Pizza Prosciutto",
+		ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+		price: 18,
+		photoName: "pizzas/prosciutto.jpg",
+		soldOut: false,
+	},
+];
+
+function Footer() {
+	const hour = new Date().getHours();
+	const openHour = 12;
+	const closeHour = 22;
+
+	const isopen = hour >= openHour && hour <= closeHour ? true : false;
+	return (
+		// <footer style={{ color: "blue" }}>{new Date().toLocaleTimeString()}</footer>
+		<footer className="footer">
+			{isopen ? (
+				<Order closeHour={closeHour} />
+			) : (
+				<p>
+					We're happy to welcome you between {openHour}:00 and {closeHour}:00
+				</p>
+			)}
+		</footer>
+	);
+}
+
+function Order(props) {
+	console.log(props);
+	return (
+		<div className="order">
+			<p>
+				we're open until {props.closeHour}:00. come visit us or order online.
+			</p>
+			<button className="btn">Order</button>
+		</div>
+	);
+}
+
+function Header() {
+	return (
+		<header className="header">
+			<h1>Fast React Pizza Co.</h1>
+		</header>
+	);
+}
+
+function App() {
+	return (
+		<header className="container">
+			<Header />
+			<Menu />
+			<Footer />
+		</header>
+	);
+}
+
+function Menu() {
+	const pizza = pizzaData;
+	const numpizza = pizza.length;
+	if (!numpizza) return <p>We're still working on our menu</p>;
+	return (
+		<main className="menu">
+			<h2>Our Menu</h2>
+			{numpizza >= 1 ? (
+				<>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti,
+						ad.
+					</p>
+					<ul className="pizzas">
+						{pizzaData.map((pizza) => (
+							<Pizza pizzaObj={pizza} key={pizza.name} />
+						))}
+					</ul>
+				</>
+			) : (
+				<p>We're still working on our menu</p>
+			)}
+		</main>
+	);
+}
+
+function Pizza({ pizzaObj }) {
+	return (
+		<li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
+			<img src={pizzaObj.photoName} alt={pizzaObj.name} />
+			<div>
+				<h3>{pizzaObj.name}</h3>
+				<p>{pizzaObj.ingredients}</p>
+				<span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
+			</div>
+		</li>
+	);
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
