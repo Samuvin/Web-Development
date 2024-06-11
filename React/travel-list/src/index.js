d0f1d6b9fd932f71c4106d41ee8ca56498d6ef85
admin <samuvin.j@gmail.com>
2024-06-11 14:29:08 +0530

    travel-app

--- React/travel-list/src/index.js
+++ React/travel-list/src/index.js
@@ -1,31 +1,11 @@
-<<<<<<< HEAD
-import React, { StrictMode } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import "./index.css";
+import App from "./components/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-	<StrictMode>
-		<App />
-	</StrictMode>
-);
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
->>>>>>> 24fe82dfd58135b9e44b7ca2b173e09012b0948b
