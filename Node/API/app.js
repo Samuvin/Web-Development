c36e9a6b04537b6c24782765e5a0c5033f098bfb
admin <samuvin.j@gmail.com>
2024-07-04 13:19:06 +0530

    Tours_API

--- Node/API/app.js
+++ Node/API/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const fs = require('fs');
 const app = express();
 
 // app.get('/', (req, res) => {
@@ -9,9 +9,81 @@ const app = express();
 // app.post('/', (req, res) => {
 //   res.status(200).send('Post Method');
 // });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.use(express.json());
 
 app.get('/api/v1/tours', (req, res) => {
-  res.status(200).json({});
+  res.status(200).json({
+    status: 'success',
+    result: tours.length,
+    data: { tours },
+  });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'failed',
+      message: 'No tour found with that ID',
+    });
+  }
+  const tour = tours.find((el) => el.id === id);
+  console.log(tour);
+  res.status(200).json({
+    status: 'success',
+    data: { tour },
+  });
+});
+
+app.patch(`/api/v1/tours/:id`, (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'failed',
+      message: 'No tour found with that ID',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>',
+    },
+  });
+});
+
+app.delete(`/api/v1/tours/:id`, (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'failed',
+      message: 'No tour found with that ID',
+    });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
+app.post(`/api/v1/tours`, (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  // const newTour = { ...req.body, newId };
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const Port = 3000;
